55625daf3c1ca64cdfface0e22e1e991d5fb3aa1
varun1492006 <varun1492006@gmail.com>
2023-06-22 21:51:19 +0530

    bug fix

--- src/app/api/candidate/route.ts
+++ src/app/api/candidate/route.ts
@@ -1,19 +1,19 @@
+import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export default async function GET(req: NextRequest){
- const { searchParams } = new URL(req.url);
+export default async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
   const admissionNo = searchParams.get("admissionNo");
   try {
     const data = await prisma.student.findUnique({
       where: { admissionNo },
       include: {
         votes: true,
-        candidate: true
+        candidate: true,
       },
     });
-    return NextResponse.json(data.candidate)
-  }
-  catch(err){
-    return NextResponse.json({"err"}, {status: 500})
+    return NextResponse.json(data);
+  } catch (err) {
+    return NextResponse.json({ err }, { status: 500 });
   }
 }
